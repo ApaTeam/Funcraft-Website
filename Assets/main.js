a7772b0ea167d143483f1c68d4295580da3f3ea4
yuriko192 <ivanwijaya19@yahoo.co.id>
2021-04-29 21:11:33 +0700

    button functions update

---
+++ Assets/main.js
@@ -0,0 +1,10 @@
+function ToFunCraft() {
+    if (
+        confirm(
+            "Opening FunCraft Application....\n\nThis application is only a prototype and not meant for the competition nor to be included in the scoring process. Do you wish to continue ?"
+        )
+    ) {
+        window.location = "https://funcraft.netlify.app/";
+    } else {
+    }
+}
