dd42a9bcaf63b1ee559eee65bf4b5d8a9bc19f9a
yuriko192 <ivanwijaya19@yahoo.co.id>
2021-05-31 21:45:55 +0700

    changed for Apa Team

--- Assets/main.js
+++ Assets/main.js
@@ -1,7 +1,7 @@
 function ToFunCraft() {
     if (
         confirm(
-            "Opening FunCraft Application....\n\nThis application is only a prototype and not meant for the competition nor to be included in the scoring process. Do you wish to continue ?"
+            "Opening FunCraft Application....\n\nThis application is still in development, continue ?"
         )
     ) {
         window.location = "https://funcraft.netlify.app/";
